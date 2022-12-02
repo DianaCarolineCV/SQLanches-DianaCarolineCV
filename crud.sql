b6a18424d8e4441ddebe45290fafd6e6d0c0557d
Diana Caroline <diannacaroline@gmail.com>
2022-12-02 11:01:57 -0300

    feat: finish

--- crud.sql
+++ crud.sql
@@ -1,34 +1,60 @@
--- Aqui você deve colocar os códigos SQL referentes às
--- Simulações de um CRUD
+INSERT INTO
+    clientes (nome, lealdade)
+VALUES
+    ('Georgia', 0);
 
--- Criação
+INSERT INTO
+    pedidos (status, cliente_id)
+VALUES
+    ('Recebido', 6);
 
--- 1)
+INSERT INTO
+    produtos_pedidos (pedido_id, produto_id)
+VALUES
+    (6, 1),
+    (6, 2),
+    (6, 6),
+    (6, 8),
+    (6, 8);
 
+SELECT
+    c.id,
+    c.nome,
+    c.lealdade,
+    p1.id,
+    p1.status,
+    p1.cliente_id,
+    p2.id,
+    p2.nome,
+    p2.tipo,
+    ROUND((p2.preco) :: NUMERIC, 2),
+    p2.pts_de_lealdade
+FROM
+    produtos_pedidos pp
+    INNER JOIN pedidos p1 ON p1.id = pp.pedido_id
+    INNER JOIN produtos p2 ON p2.id = pp.produto_id
+    INNER JOIN clientes c ON c.id = pp.pedido_id
+WHERE
+    c.nome LIKE 'Georgia';
 
+UPDATE
+    clientes
+SET
+    lealdade = (
+        SELECT
+            SUM(p2.pts_de_lealdade)
+        FROM
+            produtos_pedidos pp
+            INNER JOIN pedidos p1 ON p1.id = pp.pedido_id
+            INNER JOIN produtos p2 ON p2.id = pp.produto_id
+            INNER JOIN clientes c ON c.id = pp.pedido_id
+        WHERE
+            c.nome LIKE 'Georgia'
+    )
+WHERE
+    clientes.nome LIKE 'Georgia';
 
--- 2)
-
-
-
--- 3)
-
-
-
--- Leitura
-
--- 1)
-
-
-
--- Atualização
-
--- 1)
-
-
-
--- Deleção
-
--- 1)
-
-
+DELETE FROM
+    clientes
+WHERE
+    clientes.nome = 'Marcelo' RETURNING *;
--- selects.sql
+++ selects.sql
@@ -1,12 +1,50 @@
--- Aqui você deve colocar os códigos SQL referentes às
--- Seleções de dados
+SELECT
+    p1.id,
+    p1.status,
+    p1.cliente_id,
+    p2.id,
+    p2.nome,
+    p2.tipo,
+    p2.preco,
+    p2.pts_de_lealdade
+FROM
+    produtos_pedidos pp
+    INNER JOIN pedidos p1 ON pp.pedido_id = p1.id
+    INNER JOIN produtos p2 ON pp.produto_id = p2.id;
 
--- 1)
+SELECT
+    pp.pedido_id
+FROM
+    produtos_pedidos pp
+    INNER JOIN produtos p2 ON pp.produto_id = p2.id
+WHERE
+    p2.nome LIKE 'Fritas';
 
--- 2)
+SELECT
+    c.nome
+FROM
+    produtos_pedidos pp
+    INNER JOIN clientes c ON c.id = pp.pedido_id
+    INNER JOIN produtos p2 ON pp.produto_id = p2.id
+WHERE
+    p2.nome LIKE 'Fritas';
 
--- 3)
+SELECT
+    ROUND(SUM(p1.preco) :: NUMERIC, 2) custo_total_pedidos
+FROM
+    produtos_pedidos pp
+    INNER JOIN produtos p1 ON p1.id = pp.produto_id
+    INNER JOIN clientes c ON c.id = pp.pedido_id
+WHERE
+    c.nome LIKE 'Laura';
 
--- 4)
-
--- 5)
+SELECT
+    nome,
+    COUNT(*) count
+FROM
+    produtos_pedidos pp
+    INNER JOIN produtos ON produtos.id = pp.produto_id
+GROUP BY
+    produtos.nome
+ORDER BY
+    nome ASC;
